59687dee1674c89a61edfb81679ce1e2970e1821
Melissa Faugere <melissafaugere@gmail.com>
2024-04-20 22:04:18 +0200

    Card contact

--- src/components/section/contact/contact.jsx
+++ src/components/section/contact/contact.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Banner from '../../banner/banner';
 import bannerImage from '../../../assets/images/black_background.png';
+import CardContact from '../../cardContact/cardContact';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelopesBulk } from '@fortawesome/free-solid-svg-icons'; // Import de faHtml5 depuis free-solid-svg-icons
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
@@ -11,11 +12,7 @@ const Contact = () => (
     <section id='contact'>
     <Banner title='Contact' bannerBackground={bannerImage}/>
     <div id='contact_container'>
-      <div id='contact_container_links'>
-    <Link to={'https://github.com/melissafgr'} target='_blank' rel="noopener noreferrer" className='contact_container_github'><FontAwesomeIcon id='contact_container_github_icon' icon={faGithub} color="black" size='4x'/></Link>
-    <Link to={'https://fr.linkedin.com/in/melissa-faugere'} target='_blank' rel="noopener noreferrer" className='contact_container_linkedin'><FontAwesomeIcon id='contact_container_linkedin_icon' icon={faLinkedin} color="darkblue" size='4x'/></Link>
-    <Link to={'mailto:melissafaugere@gmail.com'} target='_blank' rel='noopener noreferrer' className='tooltip'><FontAwesomeIcon id='tooltipbutton' icon={faEnvelopesBulk} color="lightgray" size='4x'/><div className='tooltiptext'>melissafaugere@gmail.com</div></Link>
-    </div>
+    <CardContact className='card_contact'/>
     </div>
     </section>
   );
