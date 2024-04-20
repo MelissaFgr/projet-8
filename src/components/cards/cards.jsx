54d6d389bd5c900b7d7aa615b29cc1583f23e0ef
Melissa Faugere <melissafaugere@gmail.com>
2024-04-21 00:28:30 +0200

    Card portfolio

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './cards.scss';
 
-const Card = ({ title, content, url }) => (
-  <Link to={`${url}`} target='_blank' rel="noopener noreferrer" className='card'>
+const Card = ({ title, content, url, cover }) => (
+  <Link to={`${url}`} target='_blank' rel="noopener noreferrer" className='card' style={{ backgroundImage: `url(${cover})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', objectFit: 'cover' }}>
     <h3 className='card_title'>{title}</h3>
     <p className='card_content'>{content}</p>
   </Link>
--- src/components/section/portfolio/portfolio.jsx
+++ src/components/section/portfolio/portfolio.jsx
@@ -4,13 +4,19 @@ import bannerImage from '../../../assets/images/black_background.png';
 import projetsData from '../../../assets/doc/projets.json';
 import Card from '../../cards/cards';
 import './portfolio.scss'
+import imgP1 from '../../../assets/images/projet_1.png';
+import imgP2 from '../../../assets/images/projet_2.png';
+import imgP5 from '../../../assets/images/Projet_5.png';
+import imgP6 from '../../../assets/images/Projet_6.png';
+/*import imgP7 from '../../../assets/images/Projet_7.png';*/
 
 const Portfolio = () => (
     <section id='portfolio'>
     <Banner title='Portfolio' bannerBackground={bannerImage}/>
     <div className='portfolio_cards'>
         {projetsData.map(({ title, content, url, cover }) => (
-          <Card key={title} title={title} content={content} url={url} />
+          console.log ({cover}),
+          <Card key={title} title={title} content={content} url={url} cover={cover}/>
         ))}
       </div>
     </section>
