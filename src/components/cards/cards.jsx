0af09a7e787bad634865fd87f13f0bd68b545553
Melissa Faugere <melissafaugere@gmail.com>
2024-04-21 12:42:25 +0200

    Accessibilité

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -5,6 +5,7 @@ import './cards.scss';
 const Card = ({ title, content, url, cover }) => (
   <div>
     <Link to={`${url}`} target='_blank' rel="noopener noreferrer" className='card' style={{ backgroundImage: `url(${cover})`, backgroundSize: 'cover', backgroundPosition: 'start', backgroundRepeat: 'no-repeat', objectFit: 'cover' }}>
+      <span>{title}</span>
     </Link>
     <h3 className='card_title'>{title}</h3>
     <p className='card_content'>{content}</p>
