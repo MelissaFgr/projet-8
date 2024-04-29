8bc1d26ef4e1e85cc4130af9ec2d42acdd028fa1
Melissa Faugere <melissafaugere@gmail.com>
2024-04-29 11:41:11 +0200

    Modification portfolio

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './cards.scss';
 
-const Card = ({ title, content, url, cover, subtitle }) => (
+const Card = ({ title, content, url, cover, subtitle, description, warning }) => (
   <div>
     <Link to={`${url}`} target='_blank' rel="noopener noreferrer" className='card' style={{ backgroundImage: `url(${cover})`, backgroundSize: 'cover', backgroundPosition: 'start', backgroundRepeat: 'no-repeat', objectFit: 'cover' }}>
     </Link>
     <h3 className='card_title'>{title}</h3>
     <h3 className='card_title'>{subtitle}</h3>
+    <p className='card_content'>{description}</p>
     <p className='card_content'>{content}</p>
+    <p className='card_content'>{warning}</p>
   </div>
 
 );
--- src/components/section/portfolio/portfolio.jsx
+++ src/components/section/portfolio/portfolio.jsx
@@ -9,9 +9,9 @@ const Portfolio = () => (
     <section id='portfolio'>
     <Banner title='Portfolio' bannerBackground={bannerImage}/>
     <div className='portfolio_cards'>
-        {projetsData.map(({ title, content, url, cover, subtitle }) => (
+        {projetsData.map(({ title, content, url, cover, subtitle, description, warning }) => (
           console.log ({cover}),
-          <Card key={title} title={title} content={content} url={url} cover={cover} subtitle={subtitle}/>
+          <Card key={title} title={title} content={content} url={url} cover={cover} subtitle={subtitle} description={description} warning={warning}/>
         ))}
       </div>
     </section>
